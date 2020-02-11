1e63308eceebdc6d5657426693625f5afdcda0ea
user <mancusofrancesca@hotmail.com>
2020-02-11 10:10:39 +0000

    added some notes

--- homework.js
+++ homework.js
@@ -2,7 +2,10 @@
 // Understand function scope
 // Know the difference in between the let and const keywords
 // Brief
-// Using your knowledge about scope and variable declarations in JavaScript, look at the following code snippets and predict what the output or error will be and why. Copy the following episodes into a JavaScript file and add comments under each one detailing the reason for your predicted output.
+// Using your knowledge about scope and variable declarations in JavaScript,
+// look at the following code snippets and predict what the output or error
+// will be and why. Copy the following episodes into a JavaScript file and add
+// comments under each one detailing the reason for your predicted output.
 //
 // MVP
 
